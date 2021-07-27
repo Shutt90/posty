4d20e4ea3e71ee984411cd7df532a2dc1eb38f3b
Liam Pugh <78647249+Shutt90@users.noreply.github.com>
2021-07-27 14:01:12 +0100

    Added temporary logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-
 class LoginController extends Controller
 {
     public function index()
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\DashboardController;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,13 @@ use App\Http\Controllers\Auth\LoginController;
 |
 */
 
+Route::get('/', function() {
+    return view('home');
+})->name('home');
+
 Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
+
+Route::get('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
